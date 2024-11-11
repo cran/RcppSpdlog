b491d9ed9985db91a37314349972b31bb814c37f
Dirk Eddelbuettel <edd@debian.org>
2024-11-11 11:30:07 +0000

    version 0.0.19

--- inst/include/rcpp_sink.h
+++ inst/include/rcpp_sink.h
@@ -5,6 +5,10 @@
 // this define is important to no include another logger pulling in stdout
 #define SPDLOG_DISABLE_DEFAULT_LOGGER 1
 
+#if defined(SPDLOG_USE_STD_FORMAT) && __cplusplus < 202002L
+#error SPDLOG_USE_STD_FORMAT should only be set when compiling with C++20 or greater
+#endif
+
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/base_sink.h"
 #include "spdlog/sinks/basic_file_sink.h"
@@ -29,7 +33,13 @@ protected:
         // sending it to its final destination:
         spdlog::memory_buf_t formatted;
         spdlog::sinks::base_sink<Mutex>::formatter_->format(msg, formatted);
+
+        // When using C++20 with SPDLOG_USE_STD_FORMAT flag spdlog::memory_buf_t is an alias of std::string
+        #ifdef SPDLOG_USE_STD_FORMAT
+        Rcpp::Rcout << formatted;
+        #else
         Rcpp::Rcout << fmt::to_string(formatted);
+        #endif
     }
 
     void flush_() override {
--- src/formatter.cpp
+++ src/formatter.cpp
@@ -3,6 +3,22 @@
 
 #include <RcppSpdlog>
 
+// we accommodate C++20 where fmt::format is available as std::format
+// this defines a helper function used below
+#if defined(SPDLOG_USE_STD_FORMAT) && __cplusplus >= 202002L
+
+    template<std::size_t... S>
+    std::format_args unpack_vector(const std::vector<std::string>& vec, std::index_sequence<S...>) {
+        return std::make_format_args(vec[S]...);
+    }
+
+    template<std::size_t size>
+    std::format_args unpack_vector(const std::vector<std::string>& vec) {
+        return unpack_vector(vec, std::make_index_sequence<size>());
+    }
+
+#endif
+
 //' Simple Pass-Through Formatter to \code{fmt::format()}
 //'
 //' The C-level interface of R does not make it easy to pass \code{...}  arguments.
@@ -23,6 +39,28 @@
 std::string formatter(const std::string s, std::vector<std::string> v) {
     size_t n = v.size();
     switch (n) {
+#if defined(SPDLOG_USE_STD_FORMAT) && __cplusplus >= 202002L
+        case 0: return std::vformat(std::string_view(s), std::make_format_args());
+        case 1: return std::vformat(std::string_view(s), unpack_vector<1>(v));
+        case 2: return std::vformat(std::string_view(s), unpack_vector<2>(v));
+        case 3: return std::vformat(std::string_view(s), unpack_vector<3>(v));
+        case 4: return std::vformat(std::string_view(s), unpack_vector<4>(v));
+        case 5: return std::vformat(std::string_view(s), unpack_vector<5>(v));
+        case 6: return std::vformat(std::string_view(s), unpack_vector<6>(v));
+        case 7: return std::vformat(std::string_view(s), unpack_vector<7>(v));
+        case 8: return std::vformat(std::string_view(s), unpack_vector<8>(v));
+        case 9: return std::vformat(std::string_view(s), unpack_vector<9>(v));
+        case 10: return std::vformat(std::string_view(s), unpack_vector<10>(v));
+        case 11: return std::vformat(std::string_view(s), unpack_vector<11>(v));
+        case 12: return std::vformat(std::string_view(s), unpack_vector<12>(v));
+        case 13: return std::vformat(std::string_view(s), unpack_vector<13>(v));
+        case 14: return std::vformat(std::string_view(s), unpack_vector<14>(v));
+        case 15: return std::vformat(std::string_view(s), unpack_vector<15>(v));
+        default: {
+            Rcpp::warning("Only up to fifteen arguments support for now.");
+            return std::vformat(std::string_view(s), unpack_vector<12>(v));
+        }
+#else
         case 0: return fmt::format(s);
         case 1: return fmt::format(s, std::string(v[0]));
         case 2: return fmt::format(s, std::string(v[0]), std::string(v[1]));
@@ -43,5 +81,6 @@ std::string formatter(const std::string s, std::vector<std::string> v) {
             Rcpp::warning("Only up to fifteen arguments support for now.");
             return fmt::format(s, std::string(v[0]), std::string(v[1]), std::string(v[2]), std::string(v[3]), std::string(v[4]), std::string(v[5]), std::string(v[6]), std::string(v[7]), std::string(v[8]), std::string(v[9]), std::string(v[10]), std::string(v[11]));
         }
+#endif
     }
 }
